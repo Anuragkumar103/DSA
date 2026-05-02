b2efe792af8f1a9ea14244fdb148cde87ed51f4d
Anurag Kumar <142084822+Anuragkumar103@users.noreply.github.com>
2026-05-02 11:46:35 +0530

    Update inventory.java

--- inventory.java
+++ inventory.java
@@ -2,7 +2,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.IOException; 
 import java.util.StringTokenizer;
 
 public class inventory {
